857a290d2dbdd81d520884d50a20b6edbbc8cd3e
Barrantes <gerson.barrantes@covidien.com>
2016-07-21 17:47:44 -0600

    Mi primer Hello World

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+               Console.WriteLine("Hello World");
+            Console.WriteLine("press any key to exit");
+            Console.ReadKey();
+
+        }
+    }
+}
